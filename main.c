696a39207b941199b2859936ffde43c1fcb44ef3
Andrey123815 <ivredomon@gmail.com>
2021-10-10 17:49:29 +0300

    remove to modules

--- main.c
+++ main.c
@@ -5,7 +5,9 @@
 
 int main() {
     Coefficients equation = {};
-    double x1 = 0, y1 = 0, x2 = 1, y2 = 1, x3 = 1, y3 = -1;
+
+    double x1, y1, x2, y2, x3, y3;
+    scanf("%lf%lf%lf%lf%lf%lf", &x1, &y1, &x2, &y2, &x3, &y3);
 
     if (get_coefficients(&x1, &y1, &x2, &y2, &x3, &y3, &equation) == 1) {
         printf("Equation: %3.2f * y^2 + %3.2f * y + %3.2f", equation.square_term, equation.linear_term, equation.free_term);
