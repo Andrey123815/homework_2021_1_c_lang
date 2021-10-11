73a83d5453213f47894fa73b47079ba9de21141e
Andrey123815 <ivredomon@gmail.com>
2021-10-11 12:37:14 +0300

    fix unused parameters and coveraging test

--- gtest.cpp
+++ gtest.cpp
@@ -8,14 +8,14 @@ extern "C" {
 
 TEST(EQUATION_TEST, Null_Case) {
     double x1, x2, y2, x3, y3;
-    Coefficients equation = {}, eq_tst = {0, 0, 0, 0};
+    Coefficients equation = {};
     EXPECT_EQ(get_coefficients(&x1, NULL, &x2, &y2, &x3, &y3, &equation), -1);
 }
 
 TEST(EQUATION_TEST, Sample_Points) {
     double x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 1, y3 = -6;
 
-    Coefficients equation = {}, eq_tst = {0, 0, 0, 0};
+    Coefficients equation = {};
     EXPECT_EQ(get_coefficients(&x1, &y1, &x2, &y2, &x3, &y3, &equation), -1);
 }
 
